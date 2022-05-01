b5834732cff274a32f0ef7696e05c4cf23e83d65
kotarosz727 <kotarosz727@gmail.com>
2022-05-01 11:15:43 +0900

    file読み込み機能追加

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Parser, Debug)]
 #[clap(
     name = "Hello",
     version = "1.0.0",
     author = "Kotaro S",
-    about = "CLI app" 
+    about = "CLI app"
 )]
 struct Opts {
     #[clap(short, long)]
@@ -23,12 +23,17 @@ fn main() {
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, opts.verbose)
     } else {
-        println!("No file is specified");
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose)
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
     }
 }
