e0ac82eeffc3276999e0180c00e271ac79bb0f06
kotarosz727 <kotarosz727@gmail.com>
2022-05-01 11:07:25 +0900

    file読み取り機能追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
+#[derive(Parser, Debug)]
+#[clap(
+    name = "Hello",
+    version = "1.0.0",
+    author = "Kotaro S",
+    about = "CLI app" 
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "File")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        println!("No file is specified");
+    }
 }
